09f64f3c71e6efd4d60ebd5623a8d0fe728f1a8d
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-04 10:48:10 -0800

    updating styling

--- src/app/items/items.page.ts
+++ src/app/items/items.page.ts
@@ -1,12 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-items',
   templateUrl: 'items.page.html',
   styleUrls: ['items.page.scss']
 })
-export class ItemsPage {
+
+export class ItemsPage implements OnInit {
+
+  items = [
+    {
+      routeId: 'fsafw',
+      stopId: 'swffwf',
+      vendorId: 'stvdvdvring',
+      driverId: 'strdvding',
+      itemClaimIds: [
+        'fsfgewge',
+        'gesgege'
+      ],
+      itemClaims: [
+        'vsdevevg'
+      ],
+      route: 'evgfge',
+      deliverDate: ('2016-01-16T16:00:00'),
+      teamReferenceId: 'stridvdvdng',
+      routeName: 'stdvdvdring',
+      vendorName: 'dvd',
+      stopStatus: 'mddjyhj',
+      stopNo: 35,
+      discount: 35,
+      extPrice: 35,
+      itemPkgId: 'dvdv',
+      itemDesc1: 'strdvdving',
+      itemDesc2: 'strdvagehing',
+      itemSku: 'stritrwthng',
+      itemType: 'wrhwrht',
+      cubes: 35,
+      orderQty: 35,
+      lastOrderQty: 35,
+      exceptionQty: 35,
+      packageItemSeq: 35,
+      price: 35,
+      adjustedPrice: 35,
+      ratedValue: 35,
+      saleType: 'wrhwhwr',
+      itemStatus: 'rwghtrh',
+      isActive: true,
+      serialNumber: 'whtre',
+      sR: 'wrh',
+      refusalReason: 'wrong color',
+      notes: 'wrthrthrth',
+      isPickupOnly: true,
+      isEvenExchange: false,
+      stopMileage: 35,
+      stopRate: 35,
+      stopMileagePremium: 35,
+      updatedBy: 'wrhrwh',
+      updateDate: ('2016-01-16T16:00:00'),
+      accountName: 'wehgthtw',
+      isBillable: false,  
+    },
+    {
+      routeId: 'fgrew',
+      stopId: 'rtwgw',
+      vendorId: 'gwrgwer',
+      driverId: 'gerwgw',
+      itemClaimIds: [
+        'fsfgewge',
+        'gesgege'
+      ],
+      itemClaims: [
+        'vsdevevg'
+      ],
+      route: 'evgfge',
+      deliverDate: ('2020-01-16T16:00:00'),
+      teamReferenceId: 'stridvdvdng',
+      routeName: 'stdvdvdring',
+      vendorName: 'dvd',
+      stopStatus: 'mddjyhj',
+      stopNo: 35,
+      discount: 35,
+      extPrice: 35,
+      itemPkgId: 'dvdv',
+      itemDesc1: 'strdvdving',
+      itemDesc2: 'strdvagehing',
+      itemSku: 'stritrwthng',
+      itemType: 'wrhwrht',
+      cubes: 35,
+      orderQty: 35,
+      lastOrderQty: 35,
+      exceptionQty: 35,
+      packageItemSeq: 35,
+      price: 35,
+      adjustedPrice: 35,
+      ratedValue: 35,
+      saleType: 'wrhwhwr',
+      itemStatus: 'rwghtrh',
+      isActive: true,
+      serialNumber: 'whtre',
+      sR: 'wrh',
+      refusalReason: 'wrong color',
+      notes: 'wrthrthrth',
+      isPickupOnly: true,
+      isEvenExchange: false,
+      stopMileage: 35,
+      stopRate: 35,
+      stopMileagePremium: 35,
+      updatedBy: 'vedgve',
+      updateDate: ('2020-01-16T16:00:00'),
+      accountName: 'wehgthtw',
+      isBillable: false,  
+    }
+  ];
 
   constructor() {}
-
+  
+  ngOnInit() {
+  }
 }
