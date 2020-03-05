f14628c494159b2f6401bbab15261c3f910884e2
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-04 16:12:08 -0800

    orders html

--- src/app/items/items.page.ts
+++ src/app/items/items.page.ts
@@ -1,6 +1,109 @@
 import { Component, OnInit } from '@angular/core';
 
-
+export const Item = [
+  {
+    routeId: 'fsafw',
+    stopId: 'swffwf',
+    vendorId: 'stvdvdvring',
+    driverId: 'strdvding',
+    itemClaimIds: [
+      'fsfgewge',
+      'gesgege'
+    ],
+    itemClaims: [
+      'vsdevevg'
+    ],
+    route: 'evgfge',
+    deliverDate: ('2016-01-16T16:00:00'),
+    teamReferenceId: 'stridvdvdng',
+    routeName: 'stdvdvdring',
+    vendorName: 'dvd',
+    stopStatus: 'mddjyhj',
+    stopNo: 1,
+    discount: 35,
+    extPrice: 35,
+    itemPkgId: 'dvdv',
+    itemDesc1: 'strdvdving',
+    itemDesc2: 'strdvagehing',
+    itemSku: 'stritrwthng',
+    itemType: 'wrhwrht',
+    cubes: 35,
+    orderQty: 35,
+    lastOrderQty: 35,
+    exceptionQty: 35,
+    packageItemSeq: 35,
+    price: 35,
+    adjustedPrice: 35,
+    ratedValue: 35,
+    saleType: 'wrhwhwr',
+    itemStatus: 'rwghtrh',
+    isActive: true,
+    serialNumber: 'whtre',
+    sR: 'wrh',
+    refusalReason: 'wrong color',
+    notes: 'wrthrthrth',
+    isPickupOnly: true,
+    isEvenExchange: false,
+    stopMileage: 35,
+    stopRate: 35,
+    stopMileagePremium: 35,
+    updatedBy: 'wrhrwh',
+    updateDate: ('2016-01-16T16:00:00'),
+    accountName: 'wehgthtw',
+    isBillable: false,  
+  },
+  {
+    routeId: 'fgrew',
+    stopId: 'rtwgw',
+    vendorId: 'gwrgwer',
+    driverId: 'gerwgw',
+    itemClaimIds: [
+      'fsfgewge',
+      'gesgege'
+    ],
+    itemClaims: [
+      'vsdevevg'
+    ],
+    route: 'evgfge',
+    deliverDate: ('2020-01-16T16:00:00'),
+    teamReferenceId: 'stridvdvdng',
+    routeName: 'stdvdvdring',
+    vendorName: 'dvd',
+    stopStatus: 'mddjyhj',
+    stopNo: 2,
+    discount: 35,
+    extPrice: 35,
+    itemPkgId: 'dvdv',
+    itemDesc1: 'strdvdving',
+    itemDesc2: 'strdvagehing',
+    itemSku: 'stritrwthng',
+    itemType: 'wrhwrht',
+    cubes: 35,
+    orderQty: 35,
+    lastOrderQty: 35,
+    exceptionQty: 35,
+    packageItemSeq: 35,
+    price: 35,
+    adjustedPrice: 35,
+    ratedValue: 35,
+    saleType: 'wrhwhwr',
+    itemStatus: 'rwghtrh',
+    isActive: false,
+    serialNumber: 'whtre',
+    sR: 'wrh',
+    refusalReason: 'wrong color',
+    notes: 'wrthrthrth',
+    isPickupOnly: true,
+    isEvenExchange: false,
+    stopMileage: 35,
+    stopRate: 35,
+    stopMileagePremium: 35,
+    updatedBy: 'vedgve',
+    updateDate: ('2020-01-16T16:00:00'),
+    accountName: 'wehgthtw',
+    isBillable: true,  
+  }
+];
 
 @Component({
   selector: 'app-items',
@@ -10,114 +113,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class ItemsPage implements OnInit {
 
-  items = [
-    {
-      routeId: 'fsafw',
-      stopId: 'swffwf',
-      vendorId: 'stvdvdvring',
-      driverId: 'strdvding',
-      itemClaimIds: [
-        'fsfgewge',
-        'gesgege'
-      ],
-      itemClaims: [
-        'vsdevevg'
-      ],
-      route: 'evgfge',
-      deliverDate: ('2016-01-16T16:00:00'),
-      teamReferenceId: 'stridvdvdng',
-      routeName: 'stdvdvdring',
-      vendorName: 'dvd',
-      stopStatus: 'mddjyhj',
-      stopNo: 1,
-      discount: 35,
-      extPrice: 35,
-      itemPkgId: 'dvdv',
-      itemDesc1: 'strdvdving',
-      itemDesc2: 'strdvagehing',
-      itemSku: 'stritrwthng',
-      itemType: 'wrhwrht',
-      cubes: 35,
-      orderQty: 35,
-      lastOrderQty: 35,
-      exceptionQty: 35,
-      packageItemSeq: 35,
-      price: 35,
-      adjustedPrice: 35,
-      ratedValue: 35,
-      saleType: 'wrhwhwr',
-      itemStatus: 'rwghtrh',
-      isActive: true,
-      serialNumber: 'whtre',
-      sR: 'wrh',
-      refusalReason: 'wrong color',
-      notes: 'wrthrthrth',
-      isPickupOnly: true,
-      isEvenExchange: false,
-      stopMileage: 35,
-      stopRate: 35,
-      stopMileagePremium: 35,
-      updatedBy: 'wrhrwh',
-      updateDate: ('2016-01-16T16:00:00'),
-      accountName: 'wehgthtw',
-      isBillable: false,  
-    },
-    {
-      routeId: 'fgrew',
-      stopId: 'rtwgw',
-      vendorId: 'gwrgwer',
-      driverId: 'gerwgw',
-      itemClaimIds: [
-        'fsfgewge',
-        'gesgege'
-      ],
-      itemClaims: [
-        'vsdevevg'
-      ],
-      route: 'evgfge',
-      deliverDate: ('2020-01-16T16:00:00'),
-      teamReferenceId: 'stridvdvdng',
-      routeName: 'stdvdvdring',
-      vendorName: 'dvd',
-      stopStatus: 'mddjyhj',
-      stopNo: 2,
-      discount: 35,
-      extPrice: 35,
-      itemPkgId: 'dvdv',
-      itemDesc1: 'strdvdving',
-      itemDesc2: 'strdvagehing',
-      itemSku: 'stritrwthng',
-      itemType: 'wrhwrht',
-      cubes: 35,
-      orderQty: 35,
-      lastOrderQty: 35,
-      exceptionQty: 35,
-      packageItemSeq: 35,
-      price: 35,
-      adjustedPrice: 35,
-      ratedValue: 35,
-      saleType: 'wrhwhwr',
-      itemStatus: 'rwghtrh',
-      isActive: false,
-      serialNumber: 'whtre',
-      sR: 'wrh',
-      refusalReason: 'wrong color',
-      notes: 'wrthrthrth',
-      isPickupOnly: true,
-      isEvenExchange: false,
-      stopMileage: 35,
-      stopRate: 35,
-      stopMileagePremium: 35,
-      updatedBy: 'vedgve',
-      updateDate: ('2020-01-16T16:00:00'),
-      accountName: 'wehgthtw',
-      isBillable: true,  
-    }
-  ];
+  items = []
 
   constructor() {}
   
   ngOnInit() {
-    
+    this.items = Item
   }
 }
--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -1,4 +1,29 @@
 import { Component } from '@angular/core';
+import {  } from "module";
+import { Item } from '../items/items.page';
+
+export const Order = [
+  {
+  _id: 'sdfsd',
+  customerId: '12365',
+  salesPerson: 'Brianna',
+  status: 'active',
+  soNumber: 5472,
+  itemCount: 2,
+  items: Item,
+  orderDate: ("2020-03-04T23:38:35+00:00"),
+  pickupDate: ("2020-03-05T23:38:35+00:00"),
+  deliverDate: ("2020-03-05T23:38:35+00:00"),
+  orderAmount: 50,
+  receiptImageLocation: 'https://media.istockphoto.com/photos/shopping-receipt-picture-id901964616?k=6&m=901964616&s=612x612&w=0&h=RmFpYy9uDazil1H9aXkkrAOlCb0lQ-bHaFpdpl76o9A=',
+  receiptNotes: 'new order',
+  rating: '4',
+  clientId: '5646',
+  clientLocationId: 'fafa',
+  isPickedUp: true,
+  isDelivered: false,
+  }
+]
 
 @Component({
   selector: 'app-orders',
@@ -7,31 +32,10 @@ import { Component } from '@angular/core';
 })
 export class OrdersPage {
 
-  orders = [
-    {
-    _id: '',
-    customerId: '',
-    salesPerson: '',
-    status: '',
-    soNumber: 5472,
-    itemCount: 2,
-    items: [
-      
-    ],
-    orderDate: Date,
-    pickupDate: Date,
-    deliverDate: Date,
-    orderAmount: 50,
-    receiptImageLocation: '',
-    receiptNotes: '',
-    rating: '',
-    clientId: '',
-    clientLocationId: '',
-    isPickedUp: true,
-    isDelivered: false,
-    }
-  ]
+  orders = []
 
-  constructor() {}
+  constructor() {
+    this.orders = Order
+  }
 
 }
