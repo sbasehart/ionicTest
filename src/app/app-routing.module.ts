3308bfa9b5130a2a9336bbbd128f98957dd7a90a
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-05 15:42:31 -0800

    ionic save users locally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
+
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule', canActivate: [AuthGuard] },
   { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './auth/register/register.module#RegisterPageModule', canActivate: [AuthGuard] },
-  {
-    path: 'view-order',
-    loadChildren: () => import('./views/order/order.module').then( m => m.OrderPageModule)
-  },
-  {
-    path: 'view-item',
-    loadChildren: () => import('./views/item/item.module').then( m => m.ItemPageModule)
-  },
-  {
-    path: 'view-location',
-    loadChildren: () => import('./views/location/location.module').then( m => m.LocationPageModule)
-  },
+  { path: 'view-order', loadChildren: './views/order/order.module#OrderPageModule', canActivate: [AuthGuard] },
+  { path: 'view-location', loadChildren: './views/location/location.module#LocationPageModule', canActivate: [AuthGuard] },
+  { path: 'view-item', loadChildren: './views/item/item.module#ItemPageModulee', canActivate: [AuthGuard] },
 ];
 
 @NgModule({
