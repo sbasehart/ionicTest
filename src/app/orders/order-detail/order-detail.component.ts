27be0498f57894b2d9c15429a6ccd12e650cd7d4
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-05 13:46:41 -0800

    icons

--- src/app/orders/order-detail/order-detail.component.ts
+++ src/app/orders/order-detail/order-detail.component.ts
@@ -2,9 +2,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Order } from 'src/app/classes/orders';
 import * as _ from 'lodash';
 @Component({
-selector: 'app-order-detail',
-templateUrl: './order-detail.component.html',
-styleUrls: ['./order-detail.component.scss'],
+  selector: 'app-order-detail',
+  templateUrl: './order-detail.component.html',
+  styleUrls: ['./order-detail.component.scss'],
 })
 export class OrderDetailComponent {
   @Input() order: Order;
@@ -13,29 +13,32 @@ export class OrderDetailComponent {
   @Output() editOrderEvent = new EventEmitter<Order>();
   @Output() deleteOrderEvent = new EventEmitter<Order>();
 
-  constructor() {}
-  
-    saveOrder() {
-      if ( !this.isValidForm() ) {
-        return false;
-      }
-        this.saveOrderEvent.emit(this.order);
-      }
-      editOrder() {
-        this.editOrderEvent.emit(this.order);
-      }
-      deleteOrder() {
-        this.deleteOrderEvent.emit(this.order);
-      }
-      isValidForm() {
-        if ( _.isUndefined(this.order.id) ) {
-          console.log(`Invalid order.id: ${ this.order.id }`);
-          return false;
-        }
-        if ( _.isEmpty(this.order.id) ) {
-          console.log(`Invalid order.id: ${ this.order.id }`);
-          return false;
-        }
-        return true;
-      }
+  constructor() { }
+
+  saveOrder() {
+    if (!this.isValidForm()) {
+      return false;
+    }
+    this.saveOrderEvent.emit(this.order);
   }
+
+  editOrder() {
+    this.editOrderEvent.emit(this.order);
+  }
+
+  deleteOrder() {
+    this.deleteOrderEvent.emit(this.order);
+  }
+
+  isValidForm() {
+    if (_.isUndefined(this.order.id)) {
+      console.log(`Invalid order.id: ${this.order.id}`);
+      return false;
+    }
+    if (_.isEmpty(this.order.id)) {
+      console.log(`Invalid order.id: ${this.order.id}`);
+      return false;
+    }
+    return true;
+  }
+}
