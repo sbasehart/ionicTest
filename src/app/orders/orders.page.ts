42787023fe9191318c904344f569e91823eeb32a
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-04 16:35:04 -0800

    pulling items in orders

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {  } from "module";
 import { Item } from '../items/items.page';
 
 export const Order = [
@@ -33,9 +32,13 @@ export const Order = [
 export class OrdersPage {
 
   orders = []
+  items = []
 
   constructor() {
     this.orders = Order
+    this.items = Item
+    console.log(this.orders)
   }
+
 
 }
