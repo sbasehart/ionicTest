2c26815fc542eb1cf6a91195e702724d9547d1b0
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-11 08:25:23 -0700

    lavender

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Client } from '../classes/clients';
-import { ClientsService } from '../services/data/clients.service';
 import { Storage } from '@ionic/storage';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -10,14 +9,67 @@ import { Storage } from '@ionic/storage';
 })
 export class SettingsPage implements OnInit {
 
-  // clients: Client[] = []
-  // userId = null;
+  toggleOn = false
 
-  constructor(private clientService: ClientsService, private storage: Storage) { }
-
-  async ngOnInit() {
-  //   this.clients = this.clientService.getClients()
-  //   this.userId = this.route.snapshot.params.id || await this.storage.get("USER_ID");;
+  constructor(private storage: Storage, private alertController: AlertController, public toastController: ToastController) {
+    
   }
 
+  ngOnInit() {
+    this.storage.get('toggle').then(disc => this.toggleOn = disc);
+  }
+
+  async onClearFavorites() {
+    const alert = await this.alertController.create({
+      header: 'Please confirm',
+      message: 'Delete All Favorites?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Unfavorite cancelled');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.storage.remove('favorite')
+            this.favoritesRemoved()
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async favoritesRemoved() {
+    const toast = await this.toastController.create({
+      message: `All Favorites Removed`,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  darkToggle() {
+    // Use matchMedia to check the user preference
+    const toggle = document.querySelector('#themeToggle') as HTMLIonToggleElement;
+
+    // Listen for the toggle check/uncheck to toggle the dark class on the <body>
+    toggle.addEventListener('ionChange', (ev) => {
+      document.body.classList.toggle('dark', (<any>ev).detail.checked);
+    });
+
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addListener((e) => checkToggle(e.matches));
+
+    // Called by the media query to check/uncheck the toggle
+    function checkToggle(shouldCheck) {
+      toggle.checked = shouldCheck;
+      this.storage.set('toggle', JSON.stringify(this.toggleOn));
+      this.storage.set('toggle', this.toggleOn);
+    }
+  }
 }
