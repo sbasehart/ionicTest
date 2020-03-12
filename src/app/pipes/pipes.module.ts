020777fc65b602aa73617e790f7c59278b7058d1
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-12 10:56:34 -0700

    pulled this

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-// import { ItemSearch } from './itemSearch.pipe';
-// import { OrderSearch } from './orderSearch.pipe';
-// import { LocationSearch } from './locationSearch.pipe';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// // import { ItemSearch } from './itemSearch.pipe';
+// // import { OrderSearch } from './orderSearch.pipe';
+// // import { LocationSearch } from './locationSearch.pipe';
 
-import { SortByStopPipe } from "./sort-by-stop.pipe";
+// import { SortByStopPipe } from "./sort-by-stop.pipe";
 
-@NgModule({
-  declarations: [
-    // ItemSearch, 
-    SortByStopPipe, 
-    // OrderSearch, 
-    // LocationSearch
-  ],
-  imports: [
-  ],
-  exports: [
-    // ItemSearch, 
-    SortByStopPipe, 
-    // OrderSearch, 
-    // LocationSearch
-  ]
-})
-export class PipesModule { }
+// @NgModule({
+//   declarations: [
+//     // ItemSearch, 
+//     SortByStopPipe, 
+//     // OrderSearch, 
+//     // LocationSearch
+//   ],
+//   imports: [
+//   ],
+//   exports: [
+//     // ItemSearch, 
+//     SortByStopPipe, 
+//     // OrderSearch, 
+//     // LocationSearch
+//   ]
+// })
+// export class PipesModule { }
--- src/app/pipes/sort-by-stop.pipe.ts
+++ src/app/pipes/sort-by-stop.pipe.ts
@@ -1,12 +1,12 @@
-import { Pipe, PipeTransform } from '@angular/core';
+// import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'sortByStop'
-})
-export class SortByStopPipe implements PipeTransform {
+// @Pipe({
+//   name: 'sortByStop'
+// })
+// export class SortByStopPipe implements PipeTransform {
 
-  transform(Item: any, stopId: Number) {
-     sort((a,b) => a.stopId.localeCompare(b.stopId));
-   }
+//   transform(Item: any, stopId: Number) {
+//      sort((a,b) => a.stopId.localeCompare(b.stopId));
+//    }
 
-}
+// }
